96e063b83ee9f77ed1027e47e63ebaf8136c84e0
Faisal Khan <49758542+khan9048@users.noreply.github.com>
2022-01-11 18:16:35 +0000

    Delete resize.py

--- resize.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-image = Image.open(r'out_my_examples_1/out_fk_ali.jpg')
-new_image = image.resize((640, 480))
-new_image.save(r'out_my_examples_1/out_fk_ali_1.png')
-print(image.size) # Output: (1200, 776)
-print(new_image.size) # Output: (400, 400)
